7c3a3afa6fa58d98d6cfde925e1c68db42542be8
Mbm7280 <Mbm1216@163.com>
2021-12-11 22:15:42 +0800

    init

---
+++ src/main/java/com/echo/blog/AppRun.java
@@ -0,0 +1,23 @@
+package com.echo.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className AppRun
+ * @author Echo
+ * @description 启动类配置
+ * @updateTime 2021/12/11 21:59
+ * @version 1.0
+ */
+
+@SpringBootApplication
+public class AppRun {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppRun.class,args);
+
+    }
+
+}
